0bdf4158829bcfbd807f86d000ceb8e6fcb76e30
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2025-05-20 14:18:30 +0900

    javadocコメントの追加

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 感謝登録画面を表示するコントローラー.
+ * 管理者情報を操作するコントローラー.
  */
 @Controller
 @RequestMapping("/")
@@ -17,6 +17,12 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
+    /**
+     * 管理者情報登録画面に遷移する.
+     *
+     * @param form フォーム
+     * @return 管理者情報登録画面
+     */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form){
         return "administrator/insert";
