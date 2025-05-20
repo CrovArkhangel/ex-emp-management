9430e32bdaf6324d89a8f0ae4b13358984d592cc
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2025-05-20 14:37:30 +0900

    管理者情報登録用ルートの作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -26,5 +29,19 @@ public class AdministratorController {
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form){
         return "administrator/insert";
+    }
+
+    /**
+     * 送信された管理者情報を取得し、ログイン画面にリダイレクトする.
+     *
+     * @param form フォーム
+     * @return ログイン画面にリダイレクト
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form){
+        Administrator administrator = new Administrator();
+        BeanUtils.copyProperties(form, administrator);
+        administratorService.insert(administrator);
+        return "redirect:/";
     }
 }
